f5bb24ad237c4445d83f568f68e3d6fc14b6ef53
json <fanqingxuan@163.com>
2020-06-12 13:23:59 +0800

    后台页面修改

--- backend/copyright.php
+++ backend/copyright.php
@@ -1,7 +1,6 @@
 <?php if(!defined('__TYPECHO_ADMIN__')) exit; ?>
 <div class="typecho-foot" role="contentinfo">
     <div class="copyright">
-        <a href="http://typecho.org" class="i-logo-s">Typecho</a>
-        <p><?php _e('由 <a href="http://typecho.org">%s</a> 强力驱动, 版本 %s (%s)', $options->software, $prefixVersion, $suffixVersion); ?></p>
+	<a href="http://www.fxjson.com">Json的博客</a>,版权所有
     </div>
 </div>
