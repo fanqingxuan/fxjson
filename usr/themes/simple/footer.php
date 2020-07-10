29c0a917b2371a82b197c66bb9d61536d9f12879
Json <fanqingxuan@163.com>
2020-07-09 20:14:28 -0500

    Update footer.php
    去掉footer copy right

--- usr/themes/simple/footer.php
+++ usr/themes/simple/footer.php
@@ -1,6 +1,6 @@
 	<footer>
 	<p class="ftlinks"><?php if ($this->options->links){	$this->options->links(); } ?></p>
-	Copyright © 2018 <a href="<?php $this->options->siteurl(); ?>"><?php $this->options->title(); ?></a> 版权所有.<?php if ($this->options->beian){	$this->options->beian(); } ?><div style="display:none;"><?php $this->options->stat(); ?></div></footer><!-- end #footer -->
+	<a href="<?php $this->options->siteurl(); ?>"><?php $this->options->title(); ?></a> 版权所有.<?php if ($this->options->beian){	$this->options->beian(); } ?><div style="display:none;"><?php $this->options->stat(); ?></div></footer><!-- end #footer -->
 </main>
 <span class="water"></span>
 <?php $this->footer(); ?>
